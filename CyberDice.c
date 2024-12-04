4dfa68595c8872ae45b6ff4ac3ed6aa04b14e078
pssrby <wguangyi7@gmail.com>
2024-12-04 15:38:46 +0800

    add multicore

--- CyberDice.c
+++ CyberDice.c
@@ -1,13 +1,14 @@
 #include<stdint.h>
 #include "mpu6050_i2c.h"
 #include "max7219.h"
-enum fsm_state{
+#include "pico/multicore.h"
+typedef enum mode{
     reset, 
     sleep, 
     clock, 
     count_down, 
     dice
-};
+}mode_t;
 
 typedef enum faceup{
     XU, XD, YU, YD, ZU, ZD
@@ -18,12 +19,6 @@ struct dice_state{
     int16_t accl[3];
     int16_t gyro[3];
     int16_t tmp;
-};
-
-typedef struct fsm* fsm_t;
-struct fsm{
-    enum fsm_state current_state;
-    enum fsm_state next_state;
 };
 
 void mpu6050_read_state(dice_state_t current_state){
@@ -42,16 +37,35 @@ int dice_mode_handler(dice_state_t current_state, dice_state_t previous_state){
     return num;
 }
 
+void core1_main(){
+
+}
+
 int main(void){
     static struct dice_state dice_state_0, dice_state_1;
     dice_state_t current_state = &dice_state_0;
     dice_state_t previous_state = &dice_state_1;
     dice_state_t temp_state;
+    static mode_t working_mode;
+    working_mode = dice;
+    mpu6050_read_state(current_state);
     while(1){
         temp_state = previous_state;
         previous_state = current_state;
         current_state = temp_state;
         mpu6050_read_state(current_state);
-        dice_mode_handler(current_state, previous_state);
+        switch(working_mode){
+            case reset:
+            break;
+            case sleep:
+            break;
+            case clock:
+            break;
+            case count_down:
+            break;
+            case dice:
+                dice_mode_handler(current_state, previous_state);
+            break;
+        }
     }
 }
