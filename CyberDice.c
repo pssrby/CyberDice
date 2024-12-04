22169e5b3c5ce726dd2154d056fa4e8d3370f4db
pssrby <wguangyi7@gmail.com>
2024-12-04 13:52:17 +0800

    test max7219 module

--- CyberDice.c
+++ CyberDice.c
@@ -8,32 +8,48 @@ enum fsm_state{
     dice
 };
 
-enum dice_state{
+typedef enum faceup{
     XU, XD, YU, YD, ZU, ZD
+}faceup_t;
+
+typedef struct dice_state* dice_state_t;
+struct dice_state{
+    int16_t accl[3];
+    int16_t gyro[3];
+    int16_t tmp;
 };
 
-typedef struct fsm fsm_t;
+typedef struct fsm* fsm_t;
 struct fsm{
     enum fsm_state current_state;
     enum fsm_state next_state;
 };
 
+void mpu6050_read_state(dice_state_t current_state){
+    mpu6050_read_raw(current_state->accl, current_state->gyro, &(current_state->tmp));
+}
 
+faceup_t which_side(dice_state_t state){
+
+}
+
+int dice_mode_handler(dice_state_t current_state, dice_state_t previous_state){
+    // display a number, 
+    // either the previous displayed or increased
+    int num = 0;
+    MAX7219_disp_num(num);
+}
 
 int main(void){
-    fsm_t fsm_i;
-    fsm_i.next_state = dice;
+    static struct dice_state dice_state_0, dice_state_1;
+    dice_state_t current_state = &dice_state_0;
+    dice_state_t previous_state = &dice_state_1;
+    dice_state_t temp_state;
     while(1){
-        fsm_i.current_state = fsm_i.next_state;
-        switch (fsm_i.current_state)
-        {
-        case reset:
-            break;
-        case dice:
-            dice_mode_handler();
-            break;
-        default:
-            break;
-        }
+        temp_state = previous_state;
+        previous_state = current_state;
+        current_state = temp_state;
+        mpu6050_read_state(current_state);
+        dice_mode_handler(current_state, previous_state);
     }
 }
---
+++ CyberDice.h
@@ -0,0 +1,4 @@
+#ifndef _CYBERDICE_H_
+#define _CYBERDICE_H_
+
+#endif
