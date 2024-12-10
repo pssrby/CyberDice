46ac92e5cf70dfc98d8804b07097f3620c8a99fa
pssrby <wguangyi7@gmail.com>
2024-12-11 01:34:07 +0800

    deleted max7219.*

--- max7219.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MAX7219_H_
-#define _MAX7219_H_
-//定义Max7219端口
-#define Max7219_pinCLK 6
-#define Max7219_pinCS  7
-#define Max7219_pinDIN 8
-void MAX7219_disp_num(int num);
-void Init_MAX7219(void);
-#endif
